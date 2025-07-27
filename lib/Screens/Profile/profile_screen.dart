a5187f16f375dc6bb9ab3591b8d43507d01d4e7e
Parthsinhgit <parmarph1925@gmail.com>
2025-07-27 16:37:07 +0530

    Profile Screen Completed

--- lib/Screens/Profile/profile_screen.dart
+++ lib/Screens/Profile/profile_screen.dart
@@ -83,26 +83,24 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   _openBottom(BuildContext context){
     return showModalBottomSheet(context: context,builder:(BuildContext context){
-      return Container(
-        child: SizedBox(
-          height: 200,
-          width: 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(onPressed: (){
-                    _pickImage(ImageSource.camera);
-                  }, icon: Icon(Icons.camera_alt,size: 80,color: Colors.grey,),),
-                  IconButton(onPressed: (){
-                    _pickImage(ImageSource.gallery);
-                  }, icon: Icon(Icons.image,size: 80,color: Colors.grey,)),
-                ],
-              )
-            ],
-          ),
+      return SizedBox(
+        height: 200,
+        width: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(onPressed: (){
+                  _pickImage(ImageSource.camera);
+                }, icon: const Icon(Icons.camera_alt,size: 80,color: Colors.grey,),),
+                IconButton(onPressed: (){
+                  _pickImage(ImageSource.gallery);
+                }, icon: const Icon(Icons.image,size: 80,color: Colors.grey,)),
+              ],
+            )
+          ],
         ),
       );
     });
@@ -118,7 +116,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
        });
     }
     catch(ex){
-      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(ex.toString()),backgroundColor: Color(0XFF05AA82),));
+      // ignore: use_build_context_synchronously
+      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(ex.toString()),backgroundColor: const Color(0XFF05AA82),));
     }
   }
 }
